6991f6773b69b9985cb3309ece0f5452b5f90d1b
Jakousa <jakousa@cs.helsinki.fi>
2018-09-11 17:24:10 +0300

    exercise 14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
 
     def print_report
         puts self.name
@@ -10,5 +11,11 @@ class Brewery < ApplicationRecord
     def restart
         self.year = 2018
         puts "changed year to #{self.year}"
+    end
+
+    def average_rating
+        sum = self.ratings.inject(0.0) { |sum, rating| sum + rating.score }
+        average = sum / self.ratings.length
+        return average.round(2)
     end
 end

ddb1ab2bfb667bcf59aee8d9a952d82494c10426
Jakousa <jakousa@cs.helsinki.fi>
2018-09-11 16:24:31 +0300

    exercise 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        k = 0.0
+        self.ratings.each do |rating|
+            k += rating.score
+        end
+        return k / self.ratings.length
+    end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,6 @@ class Rating < ApplicationRecord
     belongs_to :beer
 
     def to_s
-        puts 'Lol'
         return "#{Beer.find(self.beer_id).name} #{self.score}"
     end
 end

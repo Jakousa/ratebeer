2c09da6fb3e37b87231b3b0b08582ba47ae9d24b
Jakousa <jakousa@cs.helsinki.fi>
2018-09-18 16:41:00 +0300

    Week 3 exercises 9-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_many :ratings
+  has_many :beers, through: :ratings
+  has_many :memberships
+  has_many :beer_clubs, through: :memberships
+
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
-
-  has_many :ratings
 end

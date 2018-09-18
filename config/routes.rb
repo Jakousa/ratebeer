5f3f67d9dc4ea8d5076898fdb6d8f56ffc8b1faa
Jakousa <jakousa@cs.helsinki.fi>
2018-09-18 14:54:32 +0300

    Week 3 exercise 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'breweries#index'
 
   get 'kaikki_bisset', to: 'beers#index'
+  get 'signup', to: 'users#new'
 
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end

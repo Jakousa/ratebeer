a7936f7b24502f7c3207b5119bc575236162d72c
Jakousa <jakousa@cs.helsinki.fi>
2018-09-22 14:37:07 +0300

    Week 4 exercise 9

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 include Helpers
 
 describe "User" do
-  before :each do
-    FactoryBot.create :user
+  let!(:user) { FactoryBot.create :user }
+
+  it "when signed up with good credentials, is added to the system" do
+    visit signup_path
+    fill_in('user_username', with: 'Brian')
+    fill_in('user_password', with: 'Secret55')
+    fill_in('user_password_confirmation', with: 'Secret55')
+
+    expect{
+      click_button('Create User')
+    }.to change{User.count}.by(1)
   end
 
   describe "who has signed up" do
+
     it "can signin with right credentials" do
       sign_in(username: "Pekka", password: "Foobar1")
 
@@ -21,16 +31,22 @@ describe "User" do
       expect(current_path).to eq(signin_path)
       expect(page).to have_content 'username and password do not match'
     end
-  end
 
-  it "when signed up with good credentials, is added to the system" do
-    visit signup_path
-    fill_in('user_username', with: 'Brian')
-    fill_in('user_password', with: 'Secret55')
-    fill_in('user_password_confirmation', with: 'Secret55')
-
-    expect{
-      click_button('Create User')
-    }.to change{User.count}.by(1)
+    describe "and has ratings" do
+      let(:beer) { FactoryBot.create :beer }
+  
+      before :each do
+        FactoryBot.create(:rating, beer: beer, score: 3, user: user)
+        visit user_path(user)
+      end
+      
+      it "can see their favorite style" do
+        expect(page).to have_content "Favorite style is: #{beer.style}"
+      end
+  
+      it "can see their favorite brewery" do
+        expect(page).to have_content "Favorite brewery is: #{beer.brewery.name}"
+      end
+    end
   end
 end

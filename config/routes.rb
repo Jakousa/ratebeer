69d0960f47f21a81e6afff4fc2d3aac0b06c30e6
Jakousa <jakousa@cs.helsinki.fi>
2018-09-24 12:15:54 +0300

    Week 5 exercises 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'breweries#index'
 
-  get 'places', to: 'places#index'
   post 'places', to:'places#search'
 
   get 'kaikki_bisset', to: 'beers#index'
@@ -17,4 +16,5 @@ Rails.application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  resources :places, only: [:index, :show]
 end
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -10,21 +10,41 @@ class BeermappingApi
     places
   end
 
+  def self.place(place_id)
+    place = Rails.cache.read(place_id)
+    return place if place
+
+    place = get_place(place_id)
+
+    Rails.cache.write(place_id, place, expires_in: 1.day)
+    place
+  end
+
+  def self.get_place(place_id)
+    url = "http://beermapping.com/webservice/locquery/#{key}/#{place_id}"
+    response = HTTParty.get url
+    place = response.parsed_response["bmp_locations"]["location"]
+    #return nil if place.is_a?(Hash) && place['id'] == "0"
+    Place.new(place)
+  end
+
   def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places.is_a?(Hash) and places['id'].nil?
+    return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
-    places.map do | place |
+    places.map do |place|
       Place.new(place)
     end
   end
 
   def self.key
+    raise "BEERMAPPING_APIKEY env variable not defined" if Rails.application.credentials.BEERMAPPING_APIKEY.nil?
+
     Rails.application.credentials.BEERMAPPING_APIKEY
   end
 end
